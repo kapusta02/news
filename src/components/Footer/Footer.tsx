81faab3a3fc0fe35583a915931271d1bfb6c44a0
Ставицкий Никита <ostrovsky2344@gmail.com>
2023-10-14 23:11:19 +0600

    #2 Добавление footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+export const Footer = () => {
+    return (
+        <>
+            <div className='footer-block'>
+                <p>© 2023 Copyright</p>
+            </div>
+        </>
+    )
+}
